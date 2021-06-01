cf80754b4aa098fff7c986dbcc1ef315a030b4cf
Markus <markus@johalla.de>
2021-06-01 23:45:08 +0200

    wor on program structure

--- src/main.py
+++ src/main.py
@@ -2,102 +2,130 @@ from pysm import State, StateMachine, Event
 import time
 import uasyncio as asyncio
 
-Init = State('Init')
-Wan = State('Wan')
-Measure = State('Measure')
-Sending = State('Sending')
-Sleep = State('Sleep')
+Phase1 = State('Phase1')
+Phase2 = State('Phase2')
+Phase3 = State('Phase3')
+Phase4 = State('Phase4')
+Phase5 = State('Phase5')
 
 
 sm = StateMachine('sm')
-sm.add_state(Init, initial=True)
-sm.add_state(Wan)
-sm.add_state(Measure)
-sm.add_state(Sending)
-sm.add_state(Sleep)
+sm.add_state(Phase1, initial=True)
+sm.add_state(Phase2)
+sm.add_state(Phase3)
+sm.add_state(Phase4)
+sm.add_state(Phase5)
 
-sm.add_transition(Init, Wan, events=['ToWan'])
-sm.add_transition(Wan, Measure, events=['ToMeasure'])
-sm.add_transition(Measure, Sending, events=['ToSend'])
-sm.add_transition(Sending, Sleep, events=['ToSleep'])
-sm.add_transition(Sleep, Init, events=['Wakeup'])
-
+sm.add_transition(Phase1, Phase2, events=['ToPhase2'])
+sm.add_transition(Phase2, Phase3, events=['ToPhase3'])
+sm.add_transition(Phase3, Phase4, events=['ToPhase4'])
+sm.add_transition(Phase4, Phase5, events=['ToPhase5'])
+sm.add_transition(Phase5, Phase1, events=['ToPhase1'])
 
 def on_enter(state, event):
+    print('')
     print('Enter state {0}'.format(state.name))
 
 def on_exit(state, event):
     print('Exit state {0}'.format(state.name))
 
 # Attach enter/exit handlers
-states = [Init, Wan, Measure, Sending, Sleep]
+states = [Phase1, Phase2, Phase3, Phase4]
 for state in states:
     state.handlers = {'enter': on_enter, 'exit': on_exit}
 
 sm.initialize()
 
-def atInit():
-    print('Initialising...')
+def doPhase1():
+    print('Reading config.')
     time.sleep(1)
-    sm.dispatch(Event('ToWan'))
-
-def atWan():
-    print('Connectin WAN...')
+    print('If RTC: Read & set time')
     time.sleep(1)
-    sm.dispatch(Event('ToMeasure'))
+    sm.dispatch(Event('ToPhase2'))
 
-async def atMeasure():
-    print('Measuring...')
+async def doPhase2():
     res = None
-    tasks = [atMeasureBus0(), atMeasureBus1(), atMeasureBus2()]
+    tasks = [Wan(), MeasureBus0(), MeasureBus1(), MeasureBus2()]
     try:
         res = await asyncio.gather(*tasks, return_exceptions=True)
     except asyncio.TimeoutError:  # These only happen if return_exceptions is False
         print('Timeout')  # With the default times, cancellation occurs first
     except asyncio.CancelledError:
         print('Cancelled')    
-    print('Measuring ended')
-    sm.dispatch(Event('ToSend'))
+    sm.dispatch(Event('ToPhase3'))
 
-async def atMeasureBus0():
+async def Wan():
+    print('Connectin WAN (Wifi, LoRa...)')
+    await asyncio.sleep(10)
+    print('WAN connected.')
+
+async def MeasureBus0():
     print('Measuring on Bus0')
     await asyncio.sleep(1)
     print('Done measuring on Bus0')
 
-async def atMeasureBus1():
+async def MeasureBus1():
     print('Measuring on Bus1')
-    await asyncio.sleep(2)
+    await asyncio.sleep(4)
     print('Done measuring on Bus1')
 
-async def atMeasureBus2():
+async def MeasureBus2():
     print('Measuring on Bus2')
-    await asyncio.sleep(3)
+    await asyncio.sleep(7)
     print('Done measuring on Bus2')
 
-def atSending():
-    print('Sending...')
-    time.sleep(1)
-    sm.dispatch(Event('ToSleep'))
-
-def atSleep():
-    print('Sleeping...')
+def doPhase3():
+    print('Data crunching.')
     time.sleep(2)
-    sm.dispatch(Event('Wakeup'))
+    sm.dispatch(Event('ToPhase4'))
+
+async def SendZeData():
+    print('Connecting to server...')
+    await asyncio.sleep(1)
+    print('Sending data...')
+    await asyncio.sleep(2)
+    print('Closing connection.')
+
+async def DoingDishes():
+    print('Updating web server')
+    await asyncio.sleep(2)
+    print('Do whatever')
+    await asyncio.sleep(1)
+    print('Finished the dishes.')
+
+async def doPhase4():
+    print('Sending data and doing the dishes.')
+    res = None
+    tasks = [SendZeData(), DoingDishes()]
+    try:
+        res = await asyncio.gather(*tasks, return_exceptions=True)
+    except asyncio.TimeoutError:  # These only happen if return_exceptions is False
+        print('Timeout')  # With the default times, cancellation occurs first
+    except asyncio.CancelledError:
+        print('Cancelled')    
+    sm.dispatch(Event('ToPhase5'))
+
+def doPhase5():
+    print('Deciding how to sleep.')
+    time.sleep(2)
+    sm.dispatch(Event('ToPhase1'))
 
 
 
 if __name__ == '__main__':
+
     while True:
         if sm.state == None:
-            atInit()
-        if sm.state == Init:
-            atInit()
-        elif sm.state == Wan:
-            atWan()
-        elif sm.state == Measure:
-            asyncio.run(atMeasure())
-        elif sm.state == Sending:
-            atSending()
-        elif sm.state == Sleep:
-            atSleep()
+            doPhase1()
+        if sm.state == Phase1:
+            doPhase1()
+        elif sm.state == Phase2:
+            asyncio.run(doPhase2())
+        elif sm.state == Phase3:
+            doPhase3()
+        elif sm.state == Phase4:
+            asyncio.run(doPhase4())
+        elif sm.state == Phase5:
+            doPhase5()
+
 
